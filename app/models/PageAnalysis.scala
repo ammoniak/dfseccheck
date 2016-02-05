2b45e8a9966806b9c58efe0f8e4def973043f955
Raphael Theiler <rtheiler@esheep.ch>
2016-02-05 08:23:49 +0100

    test scala

--- app/models/PageAnalysis.scala
+++ app/models/PageAnalysis.scala
@@ -3,6 +3,6 @@ package models
 /**
   * Created by peanut on 05/02/16.
   */
-class PageAnalysis {
+class PageAnalysis(val pageBEforeJS:String, val pageAfterJS:String, val networkConnections:Seq[(String,String,Map[String,String])]) {
 
 }
