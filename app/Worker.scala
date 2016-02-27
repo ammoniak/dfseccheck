f80cb9b98d2c48929c5a23bb87fdc676bbe7e4b6
Raphael Theiler <rtheiler@esheep.ch>
2016-02-27 12:38:57 +0100

    Worker test

--- app/Worker.scala
+++ app/Worker.scala
@@ -1,20 +1,31 @@
-import akka.actor.Actor
-import akka.actor.Actor.Receive
+
 import play.api._
 import play.api.libs.concurrent.Akka
 import play.api.mvc._
 
 import play.api.Logger
 import scala.concurrent.Future
-import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+import akka.actor._
+
 import scala.concurrent.duration._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.Play.current
 
 object Worker extends GlobalSettings {
 
-  Akka.system.scheduler.schedule(0 seconds, 30 minutes, testActor, "tick")
+
+  override def onStart(app: Application): Unit = {
+    val testActor = Akka.system.actorOf(MyActor.props)
+    Akka.system.scheduler.schedule(0 seconds, 5 seconds, testActor, "tick")
+  }
 }
 
+object  MyActor{
+  def props = Props[MyActor]
+}
 class MyActor extends Actor{
+  import MyActor._
   override def receive: Receive = {
     case "tick" => println("execute!")
     case _ => println("ups?")
