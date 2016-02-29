72985a8146c9d5bd6268c0a7b0fc23b50b9c90c2
Raphael Theiler <rtheiler@esheep.ch>
2016-02-29 22:28:08 +0100

    fixed sql

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -6,7 +6,7 @@ CREATE TABLE "useraccount" (
     "id" number(20) NOT NULL,
     "email" varchar(255) NOT NULL,
     "pw" varchar(255) NOT NULL,
-    "fullname" varchar(255) NOT NULL)
+    "fullname" varchar(255) NOT NULL
 );
 
 # --- !Downs
