1c12d969f95d788ee2dcaf093d850e0377f369bc
ammoniak <ammoniak@esheep.ch>
2016-02-29 15:14:06 +0100

    escape

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,15 +2,15 @@
 
 # --- !Ups
 
-CREATE TABLE useraccount (
-    id bigint(20) NOT NULL AUTO_INCREMENT,
-    email varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
-    fullname varchar(255) NOT NULL,
-    isAdmin boolean NOT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE "useraccount" (
+    "id" bigint(20) NOT NULL AUTO_INCREMENT,
+    "email" varchar(255) NOT NULL,
+    "password" varchar(255) NOT NULL,
+    "fullname" varchar(255) NOT NULL,
+    "isAdmin" boolean NOT NULL,
+    PRIMARY KEY ("id")
 );
 
 # --- !Downs
 
-DROP TABLE useraccount;
+DROP TABLE "useraccount";
